823891463033f8062a7520cd038a103b5172b8f0
Faliq <faliqtan@gmail.com>
2023-05-31 08:50:38 +0000

    Another change done to test poll SCM

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Another change?! Aww man..");
+        System.out.println("Yippee we're ending!");
     }
 
     public String getMessage() {
