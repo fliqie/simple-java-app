8875e73a058eb4d2eface9c7e9235f9ca9e2b51c
Faliq <faliqtan@gmail.com>
2023-06-01 05:33:35 +0000

    change go brr

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("what the new change?!");
+        System.out.println("Make that change baby");
     }
 
     public String getMessage() {
